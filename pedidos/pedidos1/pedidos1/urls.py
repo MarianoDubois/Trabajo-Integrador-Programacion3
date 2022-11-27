9f031c22b0a66abf5637ea6dba4c66cb46e6cf73
tadeofunes <71388800+tadeofunes@users.noreply.github.com>
2022-11-27 19:42:07 -0300

    Cambio de Pagina

--- pedidos/pedidos1/pedidos1/urls.py
+++ pedidos/pedidos1/pedidos1/urls.py
@@ -14,13 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from confApp.views import(
-    home_screen_view
+    home_screen_view,
+    products
 )
 urlpatterns = [
     
     path('admin/', admin.site.urls),
     path('',home_screen_view, name='home'),
-
+    path('products/',products,name='productos')
 ]
