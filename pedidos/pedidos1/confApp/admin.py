e827dd132c1b96a776aa1de6666cbaf83ebb0016
mateosinches <71389954+mateosinches@users.noreply.github.com>
2022-11-28 12:06:05 -0300

    admin corregido
    admin corregido

--- pedidos/pedidos1/confApp/admin.py
+++ pedidos/pedidos1/confApp/admin.py
@@ -1,1 +1,11 @@
 from django.contrib import admin
+from .models import *
+
+admin.site.register(CondicionesIva)
+admin.site.register(Detalles)
+admin.site.register(EstadosVenta)
+admin.site.register(Localidades)
+admin.site.register(Productos)
+admin.site.register(Proveedores)
+admin.site.register(UnidadMedida)
+admin.site.register(Ventas)
