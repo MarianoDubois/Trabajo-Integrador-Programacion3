00787cc12ea83f6c1f823d07c1fd88b1f94030f4
MarianoDubois <71388836+MarianoDubois@users.noreply.github.com>
2022-12-02 03:28:22 -0300

    log in de compras funcionando
    log in de compras funcionando

--- pedidos/pedidos1/pedidos1/urls.py
+++ pedidos/pedidos1/pedidos1/urls.py
@@ -1,18 +1,7 @@
-"""pedidos1 URL Configuration
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic.base import TemplateView
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from confApp.views import (
     products,
     DetalleProducto,
@@ -24,12 +13,12 @@ from confApp.views import (
     estado_entrega,
     PedidoExitoso
 )
-from django.contrib import admin
-from django.urls import path
 
 urlpatterns = [
 
     path('admin/', admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),
     path('products/', products, name='productos'),
     path('DetalleProducto/', DetalleProducto, name='DetalleDeProductos'),
     path('DetalleProducto2/', DetalleProducto2, name='DetalleDeProducto2'),
